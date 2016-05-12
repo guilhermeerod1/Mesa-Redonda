78abd11e3d973eaeaf56b12de070d722b5748c38
Guilherme Estrada Rodrigues <guilherme.e.rod@hotmail.com>
2016-05-12 11:12:41 -0300

    todo: Criar o DAO de Produto

---
+++ MesaRedonda/DAL/ProdutoDA.cs
@@ -0,0 +1,142 @@
+﻿using DL;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ProdutoDA
+    {
+
+        private String connString = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+
+        public void InserirProduto(Produto p)
+        {
+            
+            using (OracleConnection conexao = new OracleConnection(connString))
+            {
+
+                conexao.Open();
+
+                using (OracleCommand comando = new OracleCommand("InserirProduto", conexao))
+                {
+
+                    comando.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    comando.Parameters.Add("cnome", OracleDbType.Varchar2).Value = p.Nome;
+                    comando.Parameters.Add("cpreco", OracleDbType.Varchar2).Value = p.Preco;
+                    comando.Parameters.Add("cdescricao", OracleDbType.Clob).Value = p.Descricao;
+                    //comando.Parameters.Add("cIdImagem", OracleDbType.Int32).Value = p.Imagem.IdImagem;
+
+                    comando.ExecuteNonQuery();
+
+                }
+
+            }
+
+        }
+
+        public void AtualizarProduto(Produto p)
+        {
+
+            using (OracleConnection conexao = new OracleConnection(connString))
+            {
+
+                conexao.Open();
+
+                using (OracleCommand comando = new OracleCommand("AtualizarProduto", conexao))
+                {
+
+                    comando.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    comando.Parameters.Add("id", OracleDbType.Int32).Value = p.IdProduto;
+                    comando.Parameters.Add("cnome", OracleDbType.Varchar2).Value = p.Nome;
+                    comando.Parameters.Add("cpreco", OracleDbType.Varchar2).Value = p.Preco;
+                    comando.Parameters.Add("cdescricao", OracleDbType.Clob).Value = p.Descricao;
+                    //comando.Parameters.Add("cIdImagem", OracleDbType.Int32).Value = p.Imagem.IdImagem;
+
+                    comando.ExecuteNonQuery();
+
+                }
+
+            }
+
+        }
+
+        public void RemoverProduto(int id)
+        {
+
+            using (OracleConnection conexao = new OracleConnection(connString))
+            {
+
+                conexao.Open();
+
+                using (OracleCommand comando = new OracleCommand("RemoverProduto", conexao))
+                {
+
+                    comando.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    comando.Parameters.Add("id", OracleDbType.Int32).Value = id;
+
+                    comando.ExecuteNonQuery();
+
+                }
+
+            }
+
+        }
+
+        public List<Produto> Listar()
+        {
+
+            List<Produto> produtos = new List<Produto>();
+
+            using (OracleConnection conexao = new OracleConnection(connString))
+            {
+
+                conexao.Open();
+
+                string sql = "select IdProduto, Nome, Preco, Descricao, IdImagem from Produtos";
+
+                using (OracleCommand comando = new OracleCommand(sql, conexao))
+                {
+
+                    comando.CommandType = System.Data.CommandType.Text;
+                    
+                    using(OracleDataReader reader = comando.ExecuteReader())
+                    {
+                        if(reader.HasRows)
+                        {
+                            while(reader.Read())
+                            {
+                                Produto p = new Produto();
+                                p.IdProduto = Convert.ToInt32(reader["IdProduto"]);
+                                p.Nome = (string)reader["Nome"];
+                                p.Preco = Convert.ToDecimal(reader["Preco"]);                              
+                                p.Descricao = reader.GetOracleClob(reader.GetOrdinal("Descricao")).Value;
+                                //p.Imagem = new ImagemDA().Buscar(Convert.ToInt32(reader["IdImagem"]));
+
+                            }
+                        }
+                    }
+
+                    return produtos;
+
+                }
+
+            }
+
+        }
+
+        public Produto Buscar(int id)
+        {
+            return Listar().FirstOrDefault(x => x.IdProduto == id);
+        }
+
+    }
+}
