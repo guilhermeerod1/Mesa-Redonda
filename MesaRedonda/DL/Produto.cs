040477cc4ca52b13a554614d8550e0c122be41fb
Guilherme Estrada Rodrigues <guilherme.e.rod@hotmail.com>
2016-05-11 21:41:41 -0300

    Todo: Criar a entidade Produto no projeto DL

---
+++ MesaRedonda/DL/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class Produto
+    {
+
+        int IdProduto { get; set; }
+        string Nome { get; set; }
+        decimal Preco { get; set; }
+        string Descricao { get; set; }
+        //Imagem Imagem { get; set; }
+
+    }
+}
