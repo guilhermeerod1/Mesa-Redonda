c2f67c9d6a7dfe315d7c2266f3a2cf45a6477283
fasm19 <felipe_aumendes16@hotmail.comgit config --global user.name fasm19git commit -m 'Arquivos documentos e cursos adicionados'git config --global user.email felipe_aumendes16@hotmail.com>
2016-05-13 21:32:20 -0300

    commit

---
+++ MesaRedonda/DAL/ImagemDA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using DL;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DAL
+{
+    class ImagemDA
+    {
+        private String connString = System.Configuration.ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+    }
+
+    public void InserirImagem(Imagem i)
+    {
+        OracleConnection(connString)){
+
+            conexao.Open();
+
+            using (OracleCommand comando = new OracleCommand("Inserir Imagem" conexao))
+            {
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+
+               
+            }
+        }
+    }
+}
