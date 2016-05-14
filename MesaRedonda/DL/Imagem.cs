5addc663ff0a67971c063151942970b89ef85e99
Guilherme Estrada Rodrigues <guilherme.e.rod@hotmail.com>
2016-05-13 21:38:32 -0300

    Criada classe para a entidade Imagem (Felipe)

---
+++ MesaRedonda/DL/Imagem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+
+    public class Imagem
+    {
+
+        public int IdImagem { get; set; }
+        public string Caminho { get; set; }
+
+    }
+
+}
